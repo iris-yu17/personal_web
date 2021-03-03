fe8352c0dcf5198f6c722c18cc531e205d6150ed
Iris Yu <iris.yu0716@gmail.com>
2021-03-03 12:26:27 +0800

    finish most function

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 let page = 0; // 0:about me, 1:experience, 2:skills, 3:projects, 4:contact
+let carouselPage = 1; // 1:顯示前三個 2:顯示後三個
 const bar = document.querySelector("#bar");
-
+const leftArr = document.querySelector(".fa-chevron-left");
+const rightArr = document.querySelector(".fa-chevron-right");
+const carousel = document.querySelector(".carousel");
 let menuItem = document.querySelectorAll(".menu_item");
 
 // change page
@@ -15,6 +18,9 @@ for (let i = 0; i < menuItem.length; i++) {
   menuItem[i].addEventListener("click", changeMenuItemStyle);
 }
 
+leftArr.addEventListener("click", carouselSlideLeft);
+rightArr.addEventListener("click", carouselSlideRight);
+
 function moveMenuBar() {
   bar.style.top = page * 32 + 2 + "px";
 }
@@ -24,6 +30,44 @@ function changeMenuItemStyle() {
     menuItem[i].classList.remove("menu_selected");
   }
   menuItem[page].classList.add("menu_selected");
+}
+
+// 按menu控制右邊畫面
+$(".menu_item").on("click", function (e) {
+  let linkScroll;
+  if (page === 0) {
+    linkScroll = $(".about_me").offset().top;
+  } else if (page === 1) {
+    linkScroll = $(".experience").offset().top;
+  } else if (page === 2) {
+    linkScroll = $(".skills").offset().top;
+  } else if (page === 3) {
+    linkScroll = $(".projects").offset().top;
+  } else {
+    linkScroll = $(".contact").offset().top;
+  }
+
+  $("html,body").stop().animate(
+    // 加入stop() 讓選單滑動時不用等到動畫全程跑完就可以點選其他選單
+    {
+      scrollTop: linkScroll,
+    },
+    700
+  );
+});
+
+function carouselSlideLeft() {
+  if (carouselPage === 2) {
+    carousel.style.left = "0";
+    carouselPage = 1;
+  }
+}
+
+function carouselSlideRight() {
+  if (carouselPage === 1) {
+    carousel.style.left = "-330px";
+    carouselPage = 2;
+  }
 }
 
 function doTest() {
